24895e2308fadaf606eeb2f508d4f67b8c1753fd
danpatrickoneill <danpatrickoneill@gmail.com>
2020-08-02 16:15:31 -0400

    Added CRUD methods for localStorage

--- js/services/todoStorage.js
+++ js/services/todoStorage.js
@@ -105,6 +105,7 @@ angular
       },
 
       clearCompleted: function () {
+        // Could DRY up code by abstracting this later
         const deferred = $q.defer();
 
         const incompleteTodos = store.todos.filter((todo) => !todo.completed);
@@ -115,5 +116,46 @@ angular
         deferred.resolve(store.todos);
         return deferred.promise;
       },
+
+      insert: function (todo) {
+        const deferred = $q.defer();
+
+        store.todos.push(todo);
+        store._saveToLocalStorage(store.todos);
+
+        deferred.resolve(store.todos);
+        return deferred.promise;
+      },
+
+      get: function () {
+        const deferred = $q.defer();
+
+        angular.copy(store._getFromLocalStorage, store.todos);
+
+        deferred.resolve(store.todos);
+        return deferred.promise;
+      },
+
+      put: function (todo, index) {
+        const deferred = $q.defer();
+
+        store.todos[index] = todo;
+        store._saveToLocalStorage(store.todos);
+
+        deferred.resolve(store.todos);
+        return deferred.promise;
+      },
+
+      delete: function (todo) {
+        const deferred = $q.defer();
+
+        store.todos.splice(store.todos.indexOf(todo), 1);
+        store._saveToLocalStorage(store.todos);
+
+        deferred.resolve(store.todos);
+        return deferred.promise;
+      },
     };
+
+    return store;
   });
